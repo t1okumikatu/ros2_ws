bf251b7294a4cfd6f5d4fc7108cd192a690eef90
tokumikatu <t1okumikatu@gamil.com>
2024-04-21 14:56:30 +0900

    pub

--- build/image_pubsub/build/lib/image_pubsub/image_publisher.py
+++ build/image_pubsub/build/lib/image_pubsub/image_publisher.py
@@ -9,8 +9,8 @@ class ImagePublisher(Node):
   
   def __init__(self):
     super().__init__('image_publisher')
-    self.publisher_ = self.create_publisher(Image, 'video_frames', 10)
-    timer_period = 0.1  # seconds
+    self.publisher_ = self.create_publisher(Image, 'video_frames', 5) #10
+    timer_period = 0.07  # seconds  0.1
     self.timer = self.create_timer(timer_period, self.timer_callback)
     self.cap = cv2.VideoCapture(0)
     self.br = CvBridge()
@@ -18,24 +18,22 @@ class ImagePublisher(Node):
     ### --- aruco設定 --- ###
     #self.dict_aruco = aruco.Dictionary_get(aruco.DICT_4X4_50)
     #self.parameters = aruco.DetectorParameters_create()
-   
+
   def timer_callback(self):
     ret, frame = self.cap.read()
-          
+
     if ret == True:
-      """
+
       frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-      frame_edge = cv2.Canny(frame_gray, threshold1=100, threshold2=200)"""
-      """gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
-
+      """ frame_edge = cv2.Canny(frame_gray, threshold1=100, threshold2=200)
+      gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
       corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, self.dict_aruco, parameters=self.parameters)
-
-      frame_markers = aruco.drawDetectedMarkers(frame.copy(), corners, ids)"""
-      dst = cv2.resize(frame, (160,120))
+      frame_markers = aruco.drawDetectedMarkers(frame.copy(), corners, ids)
+      """
+      dst = cv2.resize(frame_gray, (400,400))
       self.publisher_.publish(self.br.cv2_to_imgmsg(dst))
- 
       self.get_logger().info('Publishing video frame')
-  
+
 def main(args=None):
   
   rclpy.init(args=args)

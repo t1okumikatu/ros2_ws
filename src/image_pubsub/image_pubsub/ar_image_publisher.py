f16beac802cdae03abe18141e318ffbd2a6fd712
tokumikatu <t1okumikatu@gmail.com>
2024-04-21 19:56:07 +0900

    Update ar_image_publisher.py

--- src/image_pubsub/image_pubsub/ar_image_publisher.py
+++ src/image_pubsub/image_pubsub/ar_image_publisher.py
@@ -3,7 +3,7 @@ from rclpy.node import Node
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 import cv2
-#from cv2 import aruco
+from cv2 import aruco
  
 class ImagePublisher(Node):
   
@@ -28,11 +28,12 @@ class ImagePublisher(Node):
       """ frame_edge = cv2.Canny(frame_gray, threshold1=100, threshold2=200)
       gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
       corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, self.dict_aruco, parameters=self.parameters)
-      frame_markers = aruco.drawDetectedMarkers(frame.copy(), corners, ids)
       """
+      frame_markers = aruco.drawDetectedMarkers(frame.copy(), corners, ids)
       #dst = cv2.resize(frame_gray, (400,400))
-      dst = cv2.resize(frame_gray, (160,120))
-      self.publisher_.publish(self.br.cv2_to_imgmsg(dst))
+      #dst = cv2.resize(frame_gray, (160,120))
+      self.publisher_.publish(self.br.cv2_to_imgmsg(frame_markers))
+      #self.publisher_.publish(self.br.cv2_to_imgmsg(dst))
       #self.get_logger().info('Publishing video frame')
       self.get_logger().info('1 フレームを送信')
 

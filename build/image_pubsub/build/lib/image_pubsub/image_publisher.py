050d9ad3d02318408d08069d2c3a47b642b5a116
tokumikatu <t1okumikatu@gmail.com>
2024-04-26 16:09:39 +0900

    pub

--- build/image_pubsub/build/lib/image_pubsub/image_publisher.py
+++ build/image_pubsub/build/lib/image_pubsub/image_publisher.py
@@ -9,8 +9,8 @@ class ImagePublisher(Node):
   
   def __init__(self):
     super().__init__('image_publisher')
-    self.publisher_ = self.create_publisher(Image, 'video_frames', 5) #10
-    timer_period = 0.07  # seconds  0.1
+    self.publisher_ = self.create_publisher(Image, 'video_frames', 10) #10
+    timer_period = 0.1  # seconds  0.1
     self.timer = self.create_timer(timer_period, self.timer_callback)
     self.cap = cv2.VideoCapture(0)
     self.br = CvBridge()
@@ -23,16 +23,19 @@ class ImagePublisher(Node):
     ret, frame = self.cap.read()
 
     if ret == True:
-
+      """
       frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-      """ frame_edge = cv2.Canny(frame_gray, threshold1=100, threshold2=200)
+      frame_edge = cv2.Canny(frame_gray, threshold1=100, threshold2=200)
       gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
       corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, self.dict_aruco, parameters=self.parameters)
       frame_markers = aruco.drawDetectedMarkers(frame.copy(), corners, ids)
       """
-      dst = cv2.resize(frame_gray, (400,400))
+      #dst = cv2.resize(frame_gray, (400,400))
+      dst = cv2.resize(frame, (160,120))
+      
       self.publisher_.publish(self.br.cv2_to_imgmsg(dst))
-      self.get_logger().info('Publishing video frame')
+      #self.get_logger().info('Publishing video frame')
+      self.get_logger().info('1 フレームを送信')
 
 def main(args=None):
   

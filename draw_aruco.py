d0933f1a48719dad6b7f2385a54af603a1c81be7
tokumikatu <t1okumikatu@gmail.com>
2024-05-01 17:09:26 +0900

    aruco

--- draw_aruco.py
+++ draw_aruco.py
@@ -7,7 +7,7 @@ size = 100
 for id in range(5):
     mark = aruco.drawMarker(dictionary, id, size)
     label = f"id_{id:02}"
-    #cv2.imshow(label, mark)
+    cv2.imshow(label, mark)
     cv2.imwrite(label + ".png", mark)
     cv2.waitKey(0)
 
--- src/image_pubsub/setup.py
+++ src/image_pubsub/setup.py
@@ -1,5 +1,5 @@
 from setuptools import find_packages, setup
-
+import os
 package_name = 'image_pubsub'
 
 setup(
@@ -11,7 +11,7 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name),
-    glob('launch/*_launch.py')),
+        glob('launch/my_launch.py')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,

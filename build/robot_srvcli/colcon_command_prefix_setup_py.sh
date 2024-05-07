eba6758a08a053c272b299e0eb191f0f025790e9
tokumikatu <t1okumikatu@gmail.com>
2024-05-07 20:48:04 +0900

    service

---
+++ build/robot_srvcli/colcon_command_prefix_setup_py.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/keime/ros2_ws/install/robot_interfaces/share/robot_interfaces/package.sh"
--- src/robot_srvcli/setup.py
+++ src/robot_srvcli/setup.py
@@ -20,6 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+        'client=robot_srvcli.client_member_function:main',
         ],
     },
 )
